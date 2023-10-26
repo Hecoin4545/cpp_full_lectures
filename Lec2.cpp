6b1b25754b24e9d2aaceab1da106b0fd08f0b970
shah het <92454514+Hecoin4545@users.noreply.github.com>
2023-10-26 20:22:53 +0530

    1 and 2 lectures

---
+++ Lec2.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    char b = 'h';
+    unsigned int a = -112;
+    cout << a << endl
+         << b << endl;
+    int ap = 2.0 / 5;
+    cout << ap << endl;
+    cout << 2.0 / 5 << endl;
+}
