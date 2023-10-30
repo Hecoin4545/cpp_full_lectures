54051889dd5eb63f709637320efde7f2871919a0
shah het <92454514+Hecoin4545@users.noreply.github.com>
2023-10-30 21:48:50 +0530

    lec 4 uploaded

---
+++ lec4.cpp
@@ -0,0 +1,275 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    // int i, j;
+    // i = 1;
+    // j = 0;
+    // while (j < 20)
+    // {
+    //     if (i < 5)
+    //     {
+    //         cout << i << ' ';
+    //         j = j + 1;
+    //         i = i + 1;
+    //     }
+    //     else
+    //     {
+    //         cout << endl;
+    //         i = 1;
+    //         j = j + 1;
+    //     }
+    // }
+
+    // int i, j;
+    // i = 3;
+    // j = 0;
+    // while (j <= 11)
+    // {
+    //     if (i >= 1)
+    //     {
+    //         cout << i << ' ';
+    //         i = i - 1;
+    //         j = j + 1;
+    //     }
+    //     else
+    //     {
+    //         cout << endl;
+    //         i = 3;
+    //         j = j + 1;
+    //     }
+    // }
+
+    // int i, j, k;
+    // i = 0;
+    // j = 0;
+    // k = 1;
+    // while (j < 9)
+    // {
+    //     if (k < 3)
+    //     {
+    //         i = i + 1;
+    //         cout << i << ' ';
+    //         k = k + 1;
+    //         j = j + 1;
+    //     }
+    //     else
+    //     {
+    //         k = 1;
+    //         i = i + 1;
+    //         cout << i << endl;
+    //         j = j + 1;
+    //     }
+    // }
+
+    // int i, k, j, n;
+    // i = 1;
+    // k = 0;
+    // j = 1;
+    // cin >> n;
+    // while (i <= n)
+    // {
+    //     if (k != j)
+    //     {
+    //         cout << "*";
+    //         k = k + 1;
+    //     }
+    //     else
+    //     {
+    //         k = 0;
+    //         j = j + 1;
+    //         i = i + 1;
+    //         cout << endl;
+    //     }
+    // }
+
+    // int i, j, k, l;
+    // cin >> l;
+    // i = 1;
+    // j = 0;
+    // k = 1;
+    // while (i <= l)
+    // {
+    //     if (j != k)
+    //     {
+    //         cout << k;
+    //         j = j + 1;
+    //     }
+    //     else
+    //     {
+    //         cout << endl;
+    //         j = 0;
+    //         k = k + 1;
+    //         i = i + 1;
+    //     }
+    // }
+
+    // int row, column, inc;
+    // row = 1;
+    // column = 0;
+    // inc = 1;
+    // while (row <= 4)
+    // {
+    //     if (row != column)
+    //     {
+    //         cout << inc << " ";
+    //         inc = inc + 1;
+    //         column = column + 1;
+    //     }
+    //     else
+    //     {
+    //         cout << endl;
+    //         row = row + 1;
+    //         column = 0;
+    //     }
+    // }
+
+    // int row, column, inc, dec;
+    // row = 1;
+    // column = 0;
+    // inc = 0;
+    // dec = 0;
+    // while (row <= 4)
+    // {
+    //     if (column != row)
+    //     {
+    //         inc = inc + 1;
+    //         cout << inc << " ";
+    //         column = column + 1;
+    //     }
+    //     else
+    //     {
+    //         column = 0;
+    //         row = row + 1;
+    //         inc = inc - dec;
+    //         dec = dec + 1;
+    //         cout << endl;
+    //     }
+    // }
+
+    // int row, column, dec;
+    // row = 1;
+    // column = 0;
+    // dec = row;
+    // while (row <= 4)
+    // {
+    //     if (row != column)
+    //     {
+    //         cout << dec << " ";
+    //         dec = dec - 1;
+    //         column = column + 1;
+    //     }
+    //     else
+    //     {
+    //         row = row + 1;
+    //         column = 0;
+    //         dec = row;
+    //         cout << endl;
+    //     }
+    // }
+
+    // int row, column;
+    // char ch;
+    // ch = 97;
+    // row = 1;
+    // column = 1;
+    // while (row <= 3)
+    // {
+    //     if (column <= 3)
+    //     {
+    //         cout << ch << ' ';
+    //         column = column + 1;
+    //     }
+    //     else
+    //     {
+    //         row = row + 1;
+    //         column = 1;
+    //         ch = ch + 1;
+    //         cout << endl;
+    //     }
+    // }
+
+    // int row, column;
+    // char inc;
+    // inc = 97;
+    // row = 1;
+    // column = 1;
+    // while (row <= 3)
+    // {
+    //     if (column <= 3)
+    //     {
+    //         cout << inc << ' ';
+    //         inc = inc + 1;
+    //         column = column + 1;
+    //     }
+    //     else
+    //     {
+    //         column = 1;
+    //         row = row + 1;
+    //         cout << endl;
+    //     }
+    // }
+
+    // int row, column;
+    // row = 1;
+    // column = 0;
+    // char ch = 97;
+    // while (row <= 3)
+    // {
+    //     if (row != column)
+    //     {
+    //         cout << ch << ' ';
+    //         column = column + 1;
+    //     }
+    //     else
+    //     {
+    //         column = 0;
+    //         row = row + 1;
+    //         ch = ch + 1;
+    //         cout << endl;
+    //     }
+    // }
+
+    // int rows, column;
+    // char ch;
+    // ch = 97;
+    // rows = 1;
+    // column = 0;
+    // while (rows <= 4)
+    // {
+    //     if (column != rows)
+    //     {
+    //         cout << ch << ' ';
+    //         column = column + 1;
+    //         ch = ch + 1;
+    //     }
+    //     else
+    //     {
+    //         column = 0;
+    //         rows = rows + 1;
+    //         cout << endl;
+    //     }
+    // }
+
+    // int row, column;
+    // row = 1;
+    // column = 0;
+    // char ch = 100;
+    // while (row <= 4)
+    // {
+    //     if (column != row)
+    //     {
+    //         cout << ch << ' ';
+    //         ch = ch + 1;
+    //         column = column + 1;
+    //     }
+    //     else
+    //     {
+    //         ch = ch - row - 1;
+    //         row = row + 1;
+    //         column = 0;
+    //         cout << endl;
+    //     }
+    // }
+}
