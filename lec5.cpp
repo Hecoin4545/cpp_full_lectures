ab0838a6656cdfa7d3d283273db076286659830d
shah het <92454514+Hecoin4545@users.noreply.github.com>
2023-11-01 23:07:44 +0530

    Lec 5 upload

---
+++ lec5.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    // int a, b;
+    // a = 3;
+    // b = 5;
+    // cout << "a&b: " << (a & b) << endl;
+    // cout << "a|b: " << (a | b) << endl;
+    // cout << "~a: " << (~a) << endl;
+    // cout << "a^b: " << (a ^ b) << endl;
+    // cout << (19 << 1) << endl;
+    // cout << (19 << 2) << endl;
+    // cout << (21 >> 1) << endl;
+    // cout << (21 >> 2) << endl;
+    // cout << (a++) << endl;
+    // cout << (++a) << endl;
+    // cout << (a--) << endl;
+    // cout << (--a) << endl;
+    int a, b = 1;
+    a = 10;
+    if (++a)
+    {
+        cout << b;
+    }
+    else
+    {
+        cout << (++b);
+    }
+}
